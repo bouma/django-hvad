5a88719ade012134af7c636e1acbe2d98b188a5f
Julien Hartmann <julien@etherdream.org>
2013-11-12 18:29:04 +0200

    Added some documentation to the new get_translation_aware_manager.

--- hvad/utils.py
+++ hvad/utils.py
@@ -29,19 +29,40 @@ def get_translation(instance, language_code=None):
 
 _translation_aware_managers = {}
 def get_translation_aware_manager(model):
+    """
+    Creates a translation-aware manager for a given model. The new manager
+    is built from model's default manager, with Manager or TranslationManager
+    bases replaced with TranslationAwareManager. This allows the translation-aware
+    manager to retain all custom functions.
+    If the default manager has not been customized (therefore, is Manager or
+    TranslationManager), a regular TranslationAwareManager is returned.
+
+    Also, the new translation-aware manager uses a translation-aware queryset built
+    on the model's default manager queryset_class setting. If not set, it will
+    default to the regular TranslationAwareQueryset.
+
+    Two calls with the same model will return the same class object, so introspection
+    functions work as intended.
+
+    NOTE: methods of model's default manager using super() will break if called
+    on the translation-aware manager. There is no way around this in Python 2.
+    Same for the translation-aware queryset's custom methods using super().
+    """
     from django.db.models import Manager
     from hvad.manager import (TranslationManager, TranslationAwareManager,
                               TranslationQueryset, TranslationAwareQueryset)
     global _translation_aware_managers
     if model in _translation_aware_managers:
         manager = _translation_aware_managers[model]()
-        manager.model = model
+        manager.model = model   # Manager's __init__ set it to None
         return manager
 
     default_manager = model._default_manager.__class__
-    if issubclass(TranslationManager, default_manager):
+    if issubclass(TranslationManager, default_manager): # Not the other way around
+        # Manager was not customized, use regular translation-aware manager
         manager_class = TranslationAwareManager
     else:
+        # Manager was customized, build a translation-aware customized manager
         bases = [TranslationAwareManager if issubclass(TranslationManager, b) else b
                  for b in default_manager.__bases__]
         try:
@@ -55,10 +76,13 @@ def get_translation_aware_manager(model):
                                    'Manager %r from model %r looks like it does.' %
                                    (default_manager.__name__, model._meta.object_name))
             raise
+
         queryset_class = getattr(model._default_manager, 'queryset_class', TranslationQueryset)
         if issubclass(TranslationQueryset, queryset_class):
+            # Queryset cas not customized, use regular translation-aware queryset
             qs_class = TranslationAwareQueryset
         else:
+            # Build a translation-aware, customized queryset
             bases = [TranslationAwareQueryset if issubclass(TranslationQueryset, b) else b
                         for b in queryset_class.__bases__]
             qs_class = type('TranslationAware' + queryset_class.__name__,
